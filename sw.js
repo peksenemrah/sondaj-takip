d0084324ba80dacfb79cac43c378634f88f24d03
peksenemrah <emrahpeksen@gmail.com>
2026-09-22 19:42:43 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Sondaj İş Takip – çevrimdışı önbellek */
-const SURUM='sondaj-v3';
-const TEMEL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const SURUM='sondaj-v4';
+const TEMEL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-512.png'];
 const CDN=/^https:\/\/(cdnjs\.cloudflare\.com|www\.gstatic\.com\/firebasejs|fonts\.googleapis\.com|fonts\.gstatic\.com|[abc]\.tile\.openstreetmap\.org)\//;
 self.addEventListener('install',e=>{e.waitUntil(caches.open(SURUM).then(c=>c.addAll(TEMEL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==SURUM).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
